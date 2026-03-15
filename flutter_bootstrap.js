69d945a76a98f54ec664a4a7175e69b6ca4dedad
wenslaus Minzi <wenslaus.Minzi@tra.go.tz>
2026-03-16 00:27:58 +0300

    Gender avatars, gender-aware voice greeting, gender field on users

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1596517067" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1665176855" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
