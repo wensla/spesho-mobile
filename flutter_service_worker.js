d532cc1374862412d14f0c2e3b4f0663c475fe3e
wenslaus Minzi <wenslaus.Minzi@tra.go.tz>
2026-03-16 09:50:07 +0300

    Remove login audio

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,5 +1,31 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
+'use strict';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
-self.addEventListener('fetch', (event) => { event.respondWith(fetch(event.request)); });
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      try {
+        await self.registration.unregister();
+      } catch (e) {
+        console.warn('Failed to unregister the service worker:', e);
+      }
+
+      try {
+        const clients = await self.clients.matchAll({
+          type: 'window',
+        });
+        // Reload clients to ensure they are not using the old service worker.
+        clients.forEach((client) => {
+          if (client.url && 'navigate' in client) {
+            client.navigate(client.url);
+          }
+        });
+      } catch (e) {
+        console.warn('Failed to navigate some service worker clients:', e);
+      }
+    })()
+  );
+});
